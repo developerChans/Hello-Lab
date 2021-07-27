d1712f905a14d29dea723f463c52b86fdd20953f
Seungyong Choi <ppyooy336@naver.com>
2021-07-27 21:58:06 +0900

    Add swagger

--- client/src/components/views/LandingPage/LandigPage.js
+++ client/src/components/views/LandingPage/LandigPage.js
@@ -2,11 +2,19 @@ import { useEffect } from "react";
 import axios from "axios";
 
 function LandingPage() {
-  useEffect(() => {
-    axios.get("/api/hello").then((response) => console.log(response.data));
-  }, []);
-
-  return <div>LandingPage 랜딩페이지</div>;
+  return (
+    <div>
+      <h1>LandingPage</h1>
+      <hr></hr> 랜딩페이지
+      <section labs="Labs">
+        Labs
+        <ul>
+          <li>연구실1</li>
+          <li>연구실2</li>
+        </ul>
+      </section>
+    </div>
+  );
 }
 
 export default LandingPage;
--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const { swaggerUi, specs } = require("./modules/swagger");
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('단국 꾸러기 코딩단')
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(specs));
+
+app.get("/", (req, res) => {
+  res.send("단국 꾸러기 코딩단");
 });
 
-app.get('/test', (req,res) => {
-    res.send('이거 예시');
+app.get("/test", (req, res) => {
+  res.send("이거 예시");
 });
 
 const port = 5000;
