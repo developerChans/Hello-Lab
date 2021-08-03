9f1816214d4d39dc246e4c88267260e18bf60537
Seungyong Choi <ppyooy336@naver.com>
2021-08-03 14:43:09 +0900

    Temp save for merge

--- server/index.js
+++ server/index.js
@@ -14,5 +14,8 @@ app.get("/test", (req, res) => {
   res.send("이거 예시");
 });
 
+const user = require("./routers/user/user.router");
+app.use("/user", user);
+
 const port = 5000;
 app.listen(port, console.log(`The application is listening on ${port}`));
---
+++ server/routers/user/user.router.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const userService = require("./user.service");
+
+router.get("/", userService.getAllUser);
+
+router.get("/test", userService.test);
+
+//삭제예정
+router.get("/register", userService.getAllUser);
+
+router.post("/register", userService.register);
+
+router.post('/login', auth, userServie)
+
+module.exports = router;
