16603eee72ff8177a8f0baeb6787ad08e9a1291f
Kim Chae Eun <kce3615@naver.com>
2021-08-03 15:33:24 +0900

    fix lab-list

--- client/src/components/views/MyPage/MyPage.js
+++ client/src/components/views/MyPage/MyPage.js
@@ -4,7 +4,35 @@ import { withRouter } from 'react-router-dom';
 import './MyPage.css';
 import '../views.css';
 
-function MyPage() {
+const labs = [
+    {
+        id: 0,
+        name: "운영체제",
+        prof: "최종무",
+        imgPath: '/img/solid/mint.png'
+       },
+    {
+        id: 1,
+        name: "보안",
+        prof: "조성제",
+        imgPath: '/img/solid/pink.png'
+       },
+    {
+        id: 2,
+        name: "모바일",
+        prof: "어쩌고",
+        imgPath: '/img/solid/yellow.png'
+    }
+];
+
+function MyPage(props) {
+
+    const onDashboardHandler = (event) => {
+        console.log(event.target);
+        props.history.push('/lab');
+        // 실제 동작 시 lab 페이지로 정보 넘겨줘야 함
+        // lab 페이지는 정보 받아서 해당 lab 페이지 출력해야 함
+    }
 
   return (
     <div className="wrap">
@@ -22,17 +50,17 @@ function MyPage() {
                 <p>User name</p>
             </div>
             <div id="my_lab">
-                <h2>내 연구실</h2>
+                <h2><span>내 연구실</span></h2>
                 <ul id="lab_list">
-                    <li>
-                        <a href="/lab" className="dashboard_link">
-                            <div className="dashboard_card">
-                                <img src="https://mblogthumb-phinf.pstatic.net/MjAxODAyMjdfMTAw/MDAxNTE5NzIxMDI1MDI5.tLRd3Ij8UO3B0u8jS6RhipagzEcRp-YNUYbmflkLrZsg.ZzcfBQqG4VI_Y6g0aEcUIsbpEDk0f4Jk2G7h7olVGskg.PNG.osy2201/9_%EC%97%B0%ED%95%9C_%EB%B0%94%EB%8B%A4%EC%83%89_%EB%B0%B0%EA%B2%BD%ED%99%94%EB%A9%B4.png?type=w800" className="lab_img"/>
-                                <p>OO 연구실</p>
-                                <p>OOO 교수</p>
+                    {labs.map((section, index) => (
+                        <li>
+                            <div onClick={onDashboardHandler} className="dashboard_card">
+                                <img className="lab_img" src={section.imgPath}/>
+                                <p className="lab_name">{section.name} 연구실</p>
+                                <p className="prof">{section.prof} 교수</p>
                             </div>
-                        </a>
-                    </li>
+                        </li>  
+                    ))}
                 </ul>
             </div>
         </div>
