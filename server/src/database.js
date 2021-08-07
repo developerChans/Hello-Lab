6990e06672643607bed6eac15371b1277aa033b3
Seungyong Choi <ppyooy336@naver.com>
2021-08-07 22:21:32 +0900

    Add db connetcion temp

---
+++ server/src/database.js
@@ -0,0 +1,8 @@
+const mysql = require("mysql");
+const conn = {
+  host: "dkkdb.cojnoghuurl7.ap-northeast-2.rds.amazonaws.com",
+  user: "admin",
+  port: "3306",
+  password: "dankkuko1!",
+  database: "dkkDB",
+};
