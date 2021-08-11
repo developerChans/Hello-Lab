ffac50592c63ca55ef0500c1131662f765372416
juy4556 <70626308+juy4556@users.noreply.github.com>
2021-08-11 23:07:59 +0900

    2021/08/01

--- server/src/app/User/userRoute.js
+++ server/src/app/User/userRoute.js
@@ -2,7 +2,9 @@ module.exports = function(app){
 const user = require("./userController");
 const LoginAuth = require("../../../config/logintAuth");
 
-//app.post('/app/users/students', user.postStudents);
+//학생 회원가입
+app.post('/app/users/students', user.postStudents);
+//학생 정보 get
 app.get('/app/users/students', user.getStudents);
 
 app.get("/test", user.test);
