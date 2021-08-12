242b075e3cb74491935a91db480050f908f473cb
Kim Chae Eun <kce3615@naver.com>
2021-08-12 22:29:37 +0900

    add client

---
+++ client/src/components/views/LabPage/tabs/LabAsk.js
@@ -0,0 +1,14 @@
+import { withRouter, useLocation } from 'react-router-dom';
+import LabBar from '../bars/LabBar';
+import LabHeader from '../bars/LabHeader';
+import LabSideBar from '../bars/LabSideBar';
+
+const LabAsk = () =>{
+    
+    return (
+        <div>
+            something
+        </div>
+    );
+}
+export default withRouter(LabAsk);
