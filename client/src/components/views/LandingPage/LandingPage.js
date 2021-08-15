b5979ca0e1d59cc678028ce6120c80be08365cf8
Kim Chae Eun <kce3615@naver.com>
2021-08-15 16:52:49 +0900

    delete elements

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -7,58 +7,10 @@ import MenuBar from "../../MenuBar/MenuBar";
 
 function LandingPage(props) {
 
-  const onLoginHandler = () => {
-    props.history.push('/login')
-  }
-
-  const onRegisterHandler = () => {
-    props.history.push('/register')
-  }
- 
-  const onMypageHandler = () => {
-    props.history.push('/mypage')
-  }
- 
-  const onNoticeHandler = () =>{
-    props.history.push('/notice')
-  }
-
-  const onRecruitmentHandler = () =>{
-    props.history.push('/recruitment')
-  }
-  
-  const onSearchHandler = (event) =>{
-    event.preventDefault();
-    props.history.push('/search')
-  }
-
   return (
     <div className="wrap">
       <MenuBar/>
-      <div id="content">
-        <h1>
-            <a href="/">
-              <div className="logo"></div>
-            </a>
-            <span className="page"></span>
-        </h1>
-        <button onClick={onMypageHandler}>마이페이지</button>
-        <div>
-          <h3>공지사항</h3>
-          <button onClick={onNoticeHandler}>+</button>
-        </div>
-        <div>
-          <h3>연구생 모집</h3>
-          <button onClick={onRecruitmentHandler}>+</button>
-        </div>
-        <form onSubmit={onSearchHandler}>
-          <label>통합검색</label>
-          <input type="text" />
-          <button type="submit">검색</button>
-        </form>
-      </div>
-      <div>
-      </div>
+   
     </div>
   );
 }
