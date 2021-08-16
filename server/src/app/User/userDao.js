94ce8c852ae935a74f4a7aaf58bc3fd4dc43b5b8
Seungyong Choi <ppyooy336@naver.com>
2021-08-16 09:15:26 +0900

    Fix userDao for execute

--- server/src/app/User/userDao.js
+++ server/src/app/User/userDao.js
@@ -1,54 +1,54 @@
 // 학생 생성
 async function insertStudentInfo(connection, insertStudentInfoParams) {
-    const insertStudentInfoQuery = `
-          INSERT INTO Student(email, name, studentId, major,phoneNumber,password,imageUrl)
+  const insertStudentInfoQuery = `
+          INSERT INTO Student(email, name, id, major,phoneNumber,password,imageUrl)
           VALUES (?, ?, ?, ?, ?, ?,?);
       `;
-    const insertStudentInfoRow = await connection.query(
-      insertStudentInfoQuery,
-      insertStudentInfoParams
-    );
-  
-    return insertStudentInfoRow;
+  const insertStudentInfoRow = await connection.query(
+    insertStudentInfoQuery,
+    insertStudentInfoParams
+  );
+
+  return insertStudentInfoRow;
 }
 
 // 학생 조회
-async function selectStudent(connection){
-    const selectStudentQuery = `
-    select name, studentId, major
+async function selectStudent(connection) {
+  const selectStudentQuery = `
+    select name, id, major
     from Student;
-    `
-    const selectStudentRow = await connection.query(selectStudentQuery);
-    return selectStudentRow[0];
+    `;
+  const selectStudentRow = await connection.query(selectStudentQuery);
+  return selectStudentRow[0];
 }
 
 // 교수 생성
 async function insertProfessorInfo(connection, insertProfessorInfoParams) {
-    const insertProfessorInfoQuery = `
+  const insertProfessorInfoQuery = `
           INSERT INTO Professor(email, name, professorId, major,phoneNumber,password,imageUrl)
           VALUES (?, ?, ?, ?, ?, ?,?);
       `;
-    const insertProfessorInfoRow = await connection.query(
-      insertProfessorInfoQuery,
-      insertProfessorInfoParams
-    );
-  
-    return insertProfessorInfoRow;
+  const insertProfessorInfoRow = await connection.query(
+    insertProfessorInfoQuery,
+    insertProfessorInfoParams
+  );
+
+  return insertProfessorInfoRow;
 }
 
 // 교수 조회
-async function selectProfessor(connection){
-    const selectProfessorQuery = `
+async function selectProfessor(connection) {
+  const selectProfessorQuery = `
     select name, professorId, major
     from Professor;
-    `
-    const selectProfessorRow = await connection.query(selectProfessorQuery);
-    return selectProfessorRow[0];
+    `;
+  const selectProfessorRow = await connection.query(selectProfessorQuery);
+  return selectProfessorRow[0];
 }
 
 module.exports = {
-    insertStudentInfo,
-    selectStudent,
-    insertProfessorInfo,
-    selectProfessor
+  insertStudentInfo,
+  selectStudent,
+  insertProfessorInfo,
+  selectProfessor,
 };
