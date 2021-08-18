26cf3fbda1ef344db336cb715f468e4b4d33bd10
Seungyong Choi <ppyooy336@naver.com>
2021-08-18 09:37:41 +0900

    Add Lab CRUD

--- server/src/app/User/userService.js
+++ server/src/app/User/userService.js
@@ -1,25 +1,63 @@
-const {pool} = require('../../../config/db');
-const userProvider = require('./userProvider');
-const userDao = require('./userDao');
+const { pool } = require("../../../config/db");
+const userProvider = require("./userProvider");
+const userDao = require("./userDao");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
-exports.createStudent = async function (email, name, studentNum,major,phoneNum,password,imageUrl){
+exports.createStudent = async function (
+  email,
+  name,
+  studentNum,
+  major,
+  phoneNum,
+  password,
+  imageUrl
+) {
   const salt = await bcrypt.genSalt(saltRounds);
   const hashedPwd = await bcrypt.hash(password, salt);
-  const insertStudentInfoParams = [email, name, studentNum,major,phoneNum,hashedPwd,imageUrl];
+  const insertStudentInfoParams = [
+    email,
+    name,
+    studentNum,
+    major,
+    phoneNum,
+    hashedPwd,
+    imageUrl,
+  ];
   const connection = await pool.getConnection(async (conn) => conn);
-  const createStudentResult = await userDao.insertStudentInfo(connection,insertStudentInfoParams);
+  const createStudentResult = await userDao.insertStudentInfo(
+    connection,
+    insertStudentInfoParams
+  );
   connection.release();
   return;
-}
+};
 
-exports.createProfessor = async function (email, name, professorNum,major,phoneNum,password,imageUrl){
+exports.createProfessor = async function (
+  email,
+  name,
+  professorNum,
+  major,
+  phoneNum,
+  password,
+  imageUrl
+) {
   const salt = await bcrypt.genSalt(saltRounds);
   const hashedPwd = await bcrypt.hash(password, salt);
-  const insertProfessorInfoParams = [email, name, professorNum,major,phoneNum,hashedPwd,imageUrl];
+  const insertProfessorInfoParams = [
+    email,
+    name,
+    professorNum,
+    major,
+    phoneNum,
+    hashedPwd,
+    imageUrl,
+  ];
   const connection = await pool.getConnection(async (conn) => conn);
-  const createProfessorResult = await userDao.insertProfessorInfo(connection,insertProfessorInfoParams);
+  const createProfessorResult = await userDao.insertProfessorInfo(
+    connection,
+    insertProfessorInfoParams
+  );
   connection.release();
   return;
-}
+};
