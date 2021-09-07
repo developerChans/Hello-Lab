9b5d2ecf14280ec032db71f73f629258a8c00f28
SeungYong Choi <ppyooy336@naver.com>
2021-09-07 17:34:28 +0900

    Fix mypageDao to get with professorId

--- server/src/app/Mypage/mypageDao.js
+++ server/src/app/Mypage/mypageDao.js
@@ -2,7 +2,7 @@ const selectStudentLabQuery = `
 SELECT l.id, l.name, u.name as pname
 FROM StudentLab s 
 JOIN LAB l ON s.labId = l.id 
-JOIN User ON u.id = l.professorId
+JOIN User u ON u.id = l.professorId
 WHERE s.studentId = ? AND l.status = 0`;
 
 module.exports = {
