b07c5da228d5503656148e19cb2e8588483da716
Kim Chae Eun <kce3615@naver.com>
2021-09-10 14:26:25 +0900

    lab 리덕스 통일

--- client/src/components/views/LabPage/main/LabNotice/LabNoticeForm.js
+++ client/src/components/views/LabPage/main/LabNotice/LabNoticeForm.js
@@ -2,7 +2,10 @@
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-const LabNoticeForm = () =>{
+import {useState} from 'react'
+import axios from 'axios'
+
+const LabNoticeForm = ({id}) =>{
     const [title, setTitle] = useState();
     const [content, setContent] = useState();
 
@@ -14,6 +17,17 @@ const LabNoticeForm = () =>{
         const data = editor.getData();
         setContent(data);
     }
+    const onClick = () =>{
+        // 데이터 보내기
+        const noticeBody = {
+            title,
+            content,
+            labId: id
+        }
+        axios.post(`/app/lab/${id}/notices`, noticeBody)
+        .then(response => console.log(response))
+    }
+
     return(
         <div className="form-wrapper">
             <input className="title-input" 
@@ -25,6 +39,7 @@ const LabNoticeForm = () =>{
             editor={ClassicEditor}
             onChange={(event, editor)=>onContentChange(event, editor)}
             />
+            <button onClick={onClick} className="submit-button">저장</button>
         </div>)
 }
 
