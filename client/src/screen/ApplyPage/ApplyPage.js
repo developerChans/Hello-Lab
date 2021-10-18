f34e9c6027ed3da4245562af8adceccc3cb092b4
ji-mango <susie000301@naver.com>
2021-10-18 20:23:02 +0900

    Add: ApplyPage

---
+++ client/src/screen/ApplyPage/ApplyPage.js
@@ -0,0 +1,93 @@
+import { withRouter } from "react-router-dom";
+import {FaCheck} from 'react-icons/fa';
+import './styles/ApplyPage.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const ApplyPage = () =>{
+     const [id, setId] = useState("");
+     const [major, setMajor] = useState("");
+     const [name, setName] = useState("");
+     const [imageURL, setImageURL] = useState("");
+     useEffect(()=> {
+         axios.get('/app/lab/1/member')
+             .then(response=>{
+
+                 setId(id)
+                 setMajor(major)
+                 setName(name)
+                 setImageURL(imageURL)
+                 console.log(name)
+             })
+     },[])
+    const test=(num)=>{
+        console.log(num)
+    }
+
+    return(
+        <div className = "total"z>
+            <div className="apply">
+                <div className="apply-title">
+                    <div id="apply-main-title">
+                        연구실 지원하기
+                    </div>
+                    <div id="apply-sub-title">
+                        경소톤 연구실
+                    </div>
+                </div>
+                <div className="apply-lab-info">
+                    <div id = "lab-type">
+                        <FaCheck size={30} color = "#000000" style={{
+                        }}/> 담당 교수 : {name}
+                    </div>
+                    <div id = "lab-type">
+                        <FaCheck size={30} color = "#000000" style={{
+                        }}/> 소속학과 : 소프트웨어학과
+                    </div>
+                    <div id = "lab-type">
+                        <FaCheck size={30} color = "#000000" style={{
+                        }}/> 연구분야 : 기계학습,딥러닝,이미지처리,IoT,헬스케어
+                    </div>
+                </div>
+                <div className="apply-info">
+                    <div id="apply-info-title">지원정보</div>
+                    <div className="apply-student-info">
+                        <div id="apply-info-explanation">
+                            - 지원자 기본 정보는 회원 정보가 입력됩니다. 회원 정보를 확인해주세요.
+                        </div>
+                        <div className = "apply-type">
+                            <div>
+                                <span id="type"><FaCheck size={30} color = "#000000" style={{
+                                }}/> 이름 </span>
+                            </div>
+                            <div>
+                                 <span id="type"><FaCheck size={30} color = "#000000" style={{
+                                 }}/> 학과 </span>
+                            </div>
+                            <div>
+                                 <span id="type"><FaCheck size={30} color = "#000000" style={{
+                                 }}/> 학번 </span>
+                            </div>
+                            <div>
+                                 <span id="type"><FaCheck size={30} color = "#000000" style={{
+                                 }}/> 휴대폰번호 </span>
+                            </div>
+                            <div className="apply-student-content">
+                                <span id="type"><FaCheck size={30} color = "#000000" style={{
+                                }}/> 하고 싶은 말 </span>
+                                <form>
+                                    <textarea
+                                        id="apply-content-input"
+                                    ></textarea>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button id="apply-btns" onClick={()=>test(1)}>지원하기</button>
+            </div>
+        </div>
+    );
+}
+
+export default withRouter(ApplyPage);
