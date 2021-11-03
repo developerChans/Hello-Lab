6acb138df26c66a8fb22133f2dabff14f8c34b6c
SeungYong Choi <ppyooy336@naver.com>
2021-11-03 15:44:23 +0900

    Delete trash database.js

--- server/database.js
+++
@@ -1,8 +0,0 @@
-const mysql = require("mysql2");
-const conn = {
-  host: "dkkdb.cojnoghuurl7.ap-northeast-2.rds.amazonaws.com",
-  user: "admin",
-  port: "3306",
-  password: "dankkuko1!",
-  database: "dkkDB",
-};
