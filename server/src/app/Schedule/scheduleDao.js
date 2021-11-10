1f558f5d1b6b9fb7007729aa855b8ba672db045c
박찬진 <68864422+developmentviusal@users.noreply.github.com>
2021-11-10 19:47:55 +0900

    Fetch schedule CRUD final1

--- server/src/app/Schedule/scheduleDao.js
+++ server/src/app/Schedule/scheduleDao.js
@@ -1,34 +1,31 @@
-// 모든 스케줄 조회 (사람 기준)
-async function callSchedules(connection, userIdx) {
-    const callScheduleListQuery = `
-    select LS.id as labScheduleId, LS.startDate as startDate, LS.finishDate as finishDate, LS.content as content, LS.location as location,
-    CT.id as calendarTopicId, CT.topic as topic, CT.color as color,
-    L.id as labId, L.name as labName, LS.status as status
+// 스케줄 조회 (사람 기준)
+async function callScheduleListEachUser(connection, userIdx) {
+    const callScheduleListEachUserQuery = `
+    select LS.id as calendarId, LS.content as content, LS.location as location, LS.startDate as start, LS.finishDate as end, CT.topic as title, CT.color as color
         from LabSchedule LS
-            join Lab L on LS.labId = L.id
-            join UserLab SL on L.id = SL.labId
             join CalendarTopic CT on LS.CalendarTopicId = CT.id
-        where SL.userId = ? and LS.status = 0 group by LS.id;`;
-    const [scheduleRows] = await connection.query(callScheduleListQuery, userIdx);
-    return scheduleRows;
-}
+        where LS.userId = ? and LS.status = 0
+        group by LS.id;
+    `;
+    const [scheduleListEachUserRow] = await connection.query(callScheduleListEachUserQuery, userIdx);
+    return scheduleListEachUserRow;
+};
 
-// 연구실 스케줄 조회 (연구실 기준)
-async function callSchedulesEachLab(connection, labIdx) {
-    const callScheduleEachLabListQuery = `
-    select LS.id as labScheduleId, LS.startDate as startDate, LS.finishDate as finishDate, LS.content as content, LS.location as location,
-    CT.id as calendarTopicId, CT.topic as topic, CT.color as color,
-    L.id as labId, L.name as labName, LS.status as status
+// 스케줄 조회(교수 기준)
+async function callScheduleListEachLab(connection, labIdx) {
+    const callScheduleListEachLabQuery = `
+    select LS.id as calendarId, LS.content as content, LS.location as location, LS.startDate as start, LS.finishDate as end, CT.topic as title, CT.color as color
         from LabSchedule LS
-            join Lab L on LS.labId = L.id
-            join CalendarTopic CT on LS.calendarTopicId = CT.id
-        where L.id = ? and LS.status = 0 group by LS.id;`;
-    const [scheduleEachLabRows] = await connection.query(callScheduleEachLabListQuery, labIdx);
-    return scheduleEachLabRows;
-}
+            join CalendarTopic CT on LS.CalendarTopicId = CT.id
+        where LS.labId = ? and LS.status = 0
+        group by LS.id;
+    `;
+    const [scheduleListEachLabRow] = await connection.query(callScheduleListEachLabQuery, labIdx);
+    return scheduleListEachLabRow;
+};
 
 // 스케줄 등록(교수 전용)
-async function createSchedule(connection, createScheduleParams) {
+async function createScheduleByProfessor(connection, createScheduleParams) {
     const createScheduleQuery = `
         insert into LabSchedule (startDate, content, labId, finishDate, calendarTopicId, location)
          VALUES (?,?,?,?,?,?);`;
@@ -36,8 +33,17 @@ async function createSchedule(connection, createScheduleParams) {
     return createScheduleRow;
 }
 
+// 스케줄 등록(개인)
+async function createScheduleByUser(connection, createScheduleParams) {
+    const createScheduleQuery = `
+        insert into LabSchedule (startDate, content, userId, finishDate, calendarTopicId, location)
+         VALUES (?,?,?,?,?,?);`;
+    const createScheduleRow = await connection.query(createScheduleQuery, createScheduleParams);
+    return createScheduleRow;
+}
+
 // 스케줄 삭제 (교수 전용) ==> patch입니다. (0이면 업로드 상태 1이면 일정 삭제)
-async function updateScheduleStatus(connection, labScheduleIdx, labIdx) { // professorIdx 대신 나중에 아이디를 받아야함 logInId <-- 이경우 프로페서 테이블을 조인해야함
+async function updateScheduleByProfessor(connection, labScheduleIdx, labIdx) { // professorIdx 대신 나중에 아이디를 받아야함 logInId <-- 이경우 프로페서 테이블을 조인해야함
     const updateScheduleStatusQuery = `
         UPDATE LabSchedule LS
         SET LS.status = 1
@@ -46,9 +52,19 @@ async function updateScheduleStatus(connection, labScheduleIdx, labIdx) { // pro
     return updateScheduleStatusRow[0];
 }
 
+// 스케줄 삭제(개인 전용)
+async function updateScheduleByUser(connection, labScheduleIdx, userIdx) { // professorIdx 대신 나중에 아이디를 받아야함 logInId <-- 이경우 프로페서 테이블을 조인해야함
+    const updateScheduleStatusQuery = `
+        UPDATE LabSchedule LS
+        SET LS.status = 1
+        where LS.id = ? and LS.userId = ?`;
+    const updateScheduleStatusRow = await connection.query(updateScheduleStatusQuery, [labScheduleIdx, userIdx]);
+    return updateScheduleStatusRow[0];
+}
 
-// 일정 수정 patch (교수전용) ==> patch입니다.
-async function changeSchedule(connection, startDate, content, finishDate, calendarTopicId, location, labScheduleIdx, labIdx) {
+
+// 일정 수정 patch (교수 전용) 
+async function changeScheduleByProfessor(connection, startDate, content, finishDate, calendarTopicId, location, labScheduleIdx, labIdx) {
     const changeScheduleQuery = `
         UPDATE LabSchedule LS
         SET LS.startDate = ? , LS. content = ?, LS.finishDate = ?, LS.calendarTopicId = ?, LS.location = ?
@@ -57,10 +73,23 @@ async function changeSchedule(connection, startDate, content, finishDate, calend
     return changeScheduleRow[0];
 }
 
+// 일정 수정 patch(개인 전용) 
+async function changeScheduleByUser(connection, startDate, content, finishDate, calendarTopicId, location, labScheduleIdx, labIdx) {
+    const changeScheduleQuery = `
+        UPDATE LabSchedule LS
+        SET LS.startDate = ? , LS. content = ?, LS.finishDate = ?, LS.calendarTopicId = ?, LS.location = ?
+        where LS.id = ? and LS.userId = ?`;
+    const changeScheduleRow = await connection.query(changeScheduleQuery, [startDate, content, finishDate, calendarTopicId, location, labScheduleIdx, userIdx]);
+    return changeScheduleRow[0];
+}
+
 module.exports = {
-    callSchedules,
-    callSchedulesEachLab,
-    createSchedule,
-    updateScheduleStatus,
-    changeSchedule
+    callScheduleListEachUser,
+    callScheduleListEachLab,
+    createScheduleByProfessor,
+    createScheduleByUser,
+    updateScheduleByProfessor,
+    updateScheduleByUser,
+    changeScheduleByProfessor,
+    changeScheduleByUser
 };
--- server/src/app/Schedule/scheduleService.js
+++ server/src/app/Schedule/scheduleService.js
@@ -10,7 +10,7 @@ const {errResponse} = require("../../../config/response");
 // const crypto = require("crypto");
 const {connect} = require("http2");
 
-exports.createSchedule = async function (startDate, content, labId, finishDate, calendarTopicId, location) {
+exports.createScheduleByProfessor = async function (startDate, content, labIdx, finishDate, calendarTopicId, location) {
     const connection = await pool.getConnection(async(conn) => conn);
     try {
 
@@ -30,8 +30,8 @@ exports.createSchedule = async function (startDate, content, labId, finishDate, 
             return errResponse(baseResponse.SCHEDULE_TOPIC_EMPTY)
         }
 
-        const insertScheduleParams = [startDate, content, labId, finishDate, calendarTopicId, location];
-        const scheduleResult = await scheduleDao.createSchedule(connection, insertScheduleParams);
+        const insertScheduleParams = [startDate, content, labIdx, finishDate, calendarTopicId, location];
+        const scheduleResult = await scheduleDao.createScheduleByProfessor(connection, insertScheduleParams);
         console.log(`추가된 내용 : ${scheduleResult[0].insertId}`);
         
         await connection.commit();
@@ -50,10 +50,50 @@ exports.createSchedule = async function (startDate, content, labId, finishDate, 
     }
 };
 
-exports.updateScheduleStatus = async function (labScheduleIdx, labIdx) {
+exports.createScheduleByUser = async function (startDate, content, userIdx, finishDate, calendarTopicId, location) {
+    const connection = await pool.getConnection(async(conn) => conn);
+    try {
+
+        if(startDate == NULL) {
+            return errResponse(baseResponse.SCHEDULE_DATE_EMPTY);
+        }
+
+        if(finishDate == NULL) {
+            return errResponse(baseResponse.SCHEDULE_DATE_EMPTY);
+        }
+
+        if(content.length < 1) {
+            return errResponse(baseResponse.SCHEDULE_CONTENT_EMPTY)
+        }
+
+        if(calendarTopicId == NULL) {
+            return errResponse(baseResponse.SCHEDULE_TOPIC_EMPTY)
+        }
+
+        const insertScheduleParams = [startDate, content, userIdx, finishDate, calendarTopicId, location];
+        const scheduleResult = await scheduleDao.createScheduleByUser(connection, insertScheduleParams);
+        console.log(`추가된 내용 : ${scheduleResult[0].insertId}`);
+        
+        await connection.commit();
+        return response(baseResponse.SUCCESS);
+
+    }   catch(err) {
+        await connection.rollback();
+        console.log(
+            `App - postSchedule Service error\n: ${err.message} \n${JSON.stringify(
+              err
+            )}`
+          );
+        return errResponse(baseResponse.DB_ERROR);
+    } finally {
+        connection.release();
+    }
+};
+
+exports.updateScheduleByProfessor = async function (labScheduleIdx, labIdx) {
     try {
         const connection = await pool.getConnection(async (conn) => conn);
-        const updateScheduleStatusResult = await scheduleDao.updateScheduleStatus(connection, labScheduleIdx, labIdx);
+        const updateScheduleStatusResult = await scheduleDao.updateScheduleByProfessor(connection, labScheduleIdx, labIdx);
         connection.release();
         //logger.SUCCESS(`해당 일정이 삭제되었습니다.\n`)
 
@@ -65,7 +105,22 @@ exports.updateScheduleStatus = async function (labScheduleIdx, labIdx) {
     }
 };
 
-exports.changeSchedule = async function (startDate, content, finishDate, calendarTopicId, location, labScheduleIdx, labIdx) {
+exports.updateScheduleByUser = async function (labScheduleIdx, userIdx) {
+    try {
+        const connection = await pool.getConnection(async (conn) => conn);
+        const updateScheduleStatusResult = await scheduleDao.updateScheduleByUser(connection, labScheduleIdx, userIdx);
+        connection.release();
+        //logger.SUCCESS(`해당 일정이 삭제되었습니다.\n`)
+
+        return response(baseResponse.SUCCESS);
+
+    } catch(err) {
+        // logger.error(`App - updateStatus Service error\n : ${err.message}`);
+        return errResponse(baseResponse.DB_ERROR);
+    }
+};
+
+exports.changeScheduleByProfessor = async function (startDate, content, finishDate, calendarTopicId, location, labScheduleIdx, labIdx) {
     const connection = await pool.getConnection(async(conn) => conn);
     try {
 
@@ -86,6 +141,43 @@ exports.changeSchedule = async function (startDate, content, finishDate, calenda
         }
 
         const changeScheduleResult = await scheduleDao.changeSchedule(connection, startDate, content, finishDate, calendarTopicId, location, labScheduleIdx, labIdx);
+        await connection.commit();
+        return response(baseResponse.SUCCESS);
+        
+    } catch(err) {
+        await connection.rollback();
+        console.log(
+            `App - changeSchedule Service error\n: ${err.message} \n${JSON.stringify(
+              err
+            )}`
+          );
+        return errResponse(baseResponse.DB_ERROR);
+    } finally {
+        connection.release();
+    }
+};
+
+exports.changeScheduleByUser = async function (startDate, content, finishDate, calendarTopicId, location, labScheduleIdx, userIdx) {
+    const connection = await pool.getConnection(async(conn) => conn);
+    try {
+
+        if(startDate == NULL) {
+            return errResponse(baseResponse.SCHEDULE_DATE_EMPTY);
+        }
+
+        if(finishDate == NULL) {
+            return errResponse(baseResponse.SCHEDULE_DATE_EMPTY);
+        }
+
+        if(content.length < 1) {
+            return errResponse(baseResponse.SCHEDULE_CONTENT_EMPTY)
+        }
+
+        if(calendarTopicId == NULL) {
+            return errResponse(baseResponse.SCHEDULE_TOPIC_EMPTY)
+        }
+
+        const changeScheduleResult = await scheduleDao.changeSchedule(connection, startDate, content, finishDate, calendarTopicId, location, labScheduleIdx, userIdx);
         await connection.commit();
         return response(baseResponse.SUCCESS);
         
